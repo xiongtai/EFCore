687fae37c9db99ef7cb02ea84385f1f7c2f12461
xiongtai <tai.xiong@pactera.com>
2018-07-24 19:25:47 +0800

    Add project files.

---
+++ Demo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new BLoggingContext())
+            {
+                db.Blogs.Add(new Blog
+                {
+                    Url = "http://blogs.msdn.com"
+                });
+                var count = db.SaveChanges();
+                Console.WriteLine($"{count} records saved to database");
+
+                Console.WriteLine();
+                Console.WriteLine("All blogs in database:");
+                foreach (var blog in db.Blogs)
+                {
+                    Console.WriteLine($" - {blog.Url}");
+                }
+            }
+        }
+    }
+}
